3a052be1759f30c5ba32cae5b1b46a768e81d425
wonedd <vinmarv@hotmail.com>
2022-07-07 11:46:41 -0300

    chore: cors

--- src/index.js
+++ src/index.js
@@ -1,98 +1,105 @@
-const express = require('express')
-const { v4 } = require('uuid'); 
+const express = require("express");
+const { v4 } = require("uuid");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+const cors = require("cors");
 
 const vehicles = [];
 
-function verifyIfExistsPlate(req, res, next){
-    const { plate } = req.body;
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  app.use(cors());
+  next();
+});
 
-    const vehicle = vehicles.find(vehicle => vehicle.plate === plate);
-     
-    if(vehicle){
-        return res.status(400).json({error: 'Vehicle already exists'});
-    }
-    
-    return next()
+function verifyIfExistsPlate(req, res, next) {
+  const { plate } = req.body;
+
+  const vehicle = vehicles.find((vehicle) => vehicle.plate === plate);
+
+  if (vehicle) {
+    return res.status(400).json({ error: "Vehicle already exists" });
   }
 
-app.post('/vehicles', verifyIfExistsPlate, (req, res) => {
-    const { name, brand, color, year, plate } = req.body
+  return next();
+}
 
-    const vehicle = {
-        name,
-        brand,
-        color,
-        year,
-        plate,
-        id: v4(),
-        price:Math.ceil(Math.random() * 100000),
-        favorite:false,
-        createdAt: new Date()
-    }
+app.post("/vehicles", verifyIfExistsPlate, (req, res) => {
+  const { name, brand, color, year, plate } = req.body;
 
-    vehicles.push(vehicle)
+  const vehicle = {
+    name,
+    brand,
+    color,
+    year,
+    plate,
+    id: v4(),
+    price: Math.ceil(Math.random() * 100000),
+    favorite: false,
+    createdAt: new Date(),
+  };
 
-    return res.status(201).json(vehicle)
-})
+  vehicles.push(vehicle);
 
-app.get('/', (req, res) => {
-    return res.json(vehicles)
-})
+  return res.status(201).json(vehicle);
+});
 
-app.put('/vehicles/:id', (req, res) => {
-    const { name, brand, color, year, plate } = req.body
+app.get("/", (req, res) => {
+  return res.json(vehicles);
+});
 
-    const { id } = req.params;
+app.put("/vehicles/:id", (req, res) => {
+  const { name, brand, color, year, plate } = req.body;
 
-    const vehicle = vehicles.find(vehicle => vehicle.id === id);
+  const { id } = req.params;
 
-    if(!vehicle){
-        return res.status(400).json({error: "Vehicle not found"})
-    }
-    
+  const vehicle = vehicles.find((vehicle) => vehicle.id === id);
 
-    vehicle.name = name ? name : vehicle.name;
-    vehicle.brand = brand ? brand : vehicle.brand;
-    vehicle.color = color ? color : vehicle.color;
-    vehicle.year = year ? year : vehicle.year;
-    vehicle.plate = plate ? plate : vehicle.plate;
+  if (!vehicle) {
+    return res.status(400).json({ error: "Vehicle not found" });
+  }
 
-    return res.status(200).json(vehicle)
+  vehicle.name = name ? name : vehicle.name;
+  vehicle.brand = brand ? brand : vehicle.brand;
+  vehicle.color = color ? color : vehicle.color;
+  vehicle.year = year ? year : vehicle.year;
+  vehicle.plate = plate ? plate : vehicle.plate;
 
-})
+  return res.status(200).json(vehicle);
+});
 
-app.delete('/vehicles/:id', (req, res) => {
-    const { id } = req.params
+app.delete("/vehicles/:id", (req, res) => {
+  const { id } = req.params;
 
-    const vehicle = vehicles.find(vehicle => vehicle.id === id);
+  const vehicle = vehicles.find((vehicle) => vehicle.id === id);
 
-    if(!vehicle){
-        return res.status(400).json({error: "Vehicle not found"})
-    }
+  if (!vehicle) {
+    return res.status(400).json({ error: "Vehicle not found" });
+  }
 
-    vehicles.splice(vehicles.indexOf(vehicle), 1)
+  vehicles.splice(vehicles.indexOf(vehicle), 1);
 
-    return res.status(204).send()
-})
+  return res.status(204).send();
+});
 
-app.patch('/vehicles/:id', (req, res) => {
-    const { id } = req.params;
+app.patch("/vehicles/:id", (req, res) => {
+  const { id } = req.params;
 
-    const vehicle = vehicles.find(vehicle => vehicle.id === id);
+  const vehicle = vehicles.find((vehicle) => vehicle.id === id);
 
-    if(!vehicle){
-        return res.status(400).json({error: "Vehicle not found"})
-    }
+  if (!vehicle) {
+    return res.status(400).json({ error: "Vehicle not found" });
+  }
 
-    vehicle.favorite = true;
+  vehicle.favorite = true;
 
-    return res.status(200).json(vehicle)
-})
+  return res.status(200).json(vehicle);
+});
 
-// TODO: search logic 
+// TODO: search logic
 
-app.listen(3333)
+app.listen(3333);
